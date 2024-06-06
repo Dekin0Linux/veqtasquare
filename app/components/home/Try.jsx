1323c5e7f9bb7f570d6550c7e8e94e98edf37c5f
Dekin0Linux <dekinlinux@gmail.com>
2024-06-06 10:04:09 +0000

    adding new changes

--- app/components/home/Try.jsx
+++ app/components/home/Try.jsx
@@ -6,7 +6,7 @@ function Try() {
         <div className="container mx-auto  bg-gradient-to-l from-sky-300 to-sky-600 md:p-20 p-5 rounded-2xl text-white" >
             <div>
                 <h3 className="md:text-6xl text-3xl font-bold">Let’s try our service now!</h3>
-                <p className="my-2 md:w-[900px] md:text-xl">Ready to experience the future of cryptocurrency transactions? VeqtaSquare invites you to join our platform and take advantage of our innovative services. </p>
+                <p className="my-2 md:w-[900px] md:text-xl">Ready to experience the future of cryptocurrency transactions? VeqtaSquare invites you to join our platform and take advantage of our innovative services . </p>
             </div>
 
         </div>
